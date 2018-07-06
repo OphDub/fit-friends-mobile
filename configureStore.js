7c637a4f6439509bd072a5d931e35fd3a888ae4f
Ophus Wong <ophus.wong@gmail.com>
2018-07-06 12:26:39 -0600

    set up skeleton for user saga, user reducer, and store config

--- configureStore.js
+++ configureStore.js
@@ -1,8 +1,17 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers/index';
 
+import createSagaMiddleware from 'redux-saga';
+import userSaga from './sagas/userSaga';
+
+const sagaMiddleware = createSagaMiddleware();
+const middleware = applyMiddleware(sagaMiddleware);
+
 const configureStore = () => {
-  let store = createStore(rootReducer);
+  let store = createStore(rootReducer, middleware);
+
+  sagaMiddleware.run(userSaga);
+
   return store;
 }
 
