5ece77a7089b27f0a2116a690d374992162a0b32
Ophus Wong <ophus.wong@gmail.com>
2018-07-06 12:40:04 -0600

    add general file structure for app

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
 
 import { Provider } from 'react-redux';
 import configureStore from './configureStore';
