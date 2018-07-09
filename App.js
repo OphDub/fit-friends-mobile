a735c0599049fc208277e985ad516897e32e1428
Ophus Wong <ophus.wong@gmail.com>
2018-07-09 12:45:57 -0600

    Add styling and icons for tabnav

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
+import { MaterialIcons } from '@expo/vector-icons';
 
 import { Provider } from 'react-redux';
 import configureStore from './configureStore';
@@ -19,9 +20,25 @@ const RouteConfig = {
 
 const BottomTabNavConfig = {
   initialRouteName: 'Home',
-  activeTintColor: '#f0edf6',
-  inactiveTintColor: '#3e2465',
-  barStyle: { backgroundColor: '#694fad' },
+  activeTintColor: '#48E0EA',
+  inactiveTintColor: '#f0edf6',
+  barStyle: { backgroundColor: '#3d3d3d' },
+  navigationOptions: ({ navigation }) => ({
+    tabBarIcon: ({ focused, tintColor }) => {
+      const { routeName } = navigation.state;
+      let iconName;
+
+      if (routeName === 'Home') {
+        iconName = `home`;
+      } else if (routeName === 'Team') {
+        iconName = `people`;
+      } else if (routeName === 'WorkoutHistory') {
+        iconName = `history`;
+      }
+
+      return <MaterialIcons name={iconName} size={25} color={tintColor} />;
+    },
+  })
 };
 
 const BottomTabNav = createMaterialBottomTabNavigator(RouteConfig, BottomTabNavConfig);
