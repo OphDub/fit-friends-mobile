e0859c07e393eee18cae998617c4ef555eff1cb8
Ophus Wong <ophus.wong@gmail.com>
2018-07-29 14:20:40 -0600

    Change workoutHistory to ListItems

--- containers/WorkoutHistory/WorkoutHistory.js
+++ containers/WorkoutHistory/WorkoutHistory.js
@@ -1,11 +1,60 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  View,
+} from 'react-native';
+import { ListItem } from 'react-native-elements';
+
+const mockWorkouts = [
+  {
+    title: 'my workout',
+    desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde expedita veritatis architecto adipisci nemo voluptate facilis consectetur aperiam ut ipsam vitae dolor accusamus totam eveniet modi ratione nostrum, natus ab!',
+    originator: 'Ophus',
+    exercises: {
+      a: { name: 'push ups', reps: '10', id: 1531616558129 },
+      b: { name: 'jumping jacks', reps: '10', id: 1531616558130 },
+      c: { name: 'air squats', reps: '10', id: 1531616558131 }
+    }
+  },
+  {
+    title: 'other workout',
+    desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde expedita veritatis architecto adipisci nemo voluptate facilis consectetur aperiam ut ipsam vitae dolor accusamus totam eveniet modi ratione nostrum, natus ab!',
+    originator: 'Felipe',
+    exercises: {
+      a: { name: 'squats', reps: '10', id: 1531616558129 },
+      b: { name: 'push ups', reps: '10', id: 1531616558130 },
+      c: { name: 'pull ups', reps: '10', id: 1531616558131 }
+    }
+  }
+];
 
 export default class WorkoutHistory extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      workouts: []
+    };
+  }
+
+  componentWillMount = () => {
+    this.setState({
+      workouts: mockWorkouts
+    })
+  }
+
   render() {
     return(
       <View style={styles.container}>
-        <Text>WorkoutHistory</Text>
+        {
+          this.state.workouts.map((workout, i) => (
+            <ListItem
+              key={i}
+              title={workout.title}
+              subtitle={workout.originator}
+              onPress={() => this.props.navigation.navigate('Workout', workout)}
+            />
+          ))
+        }
       </View>
     )
   }
@@ -13,9 +62,6 @@ export default class WorkoutHistory extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#FFC0CB',
   },
 });
