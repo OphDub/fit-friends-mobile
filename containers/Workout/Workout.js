917f709573522c3c69867b85ed574e67d8823fee
Ophus Wong <ophus.wong@gmail.com>
2018-07-29 14:21:22 -0600

    Change Workout to stateless component

--- containers/Workout/Workout.js
+++ containers/Workout/Workout.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 
-export default class Workout extends Component {
-  render() {
-    return(
-      <View style={styles.container}>
-        <Text>Workout</Text>
+export const Workout = ({navigation}) => {
+  const { params } = navigation.state;
+  const exerciseKeys = Object.keys(params.exercises);
+  const exerciseList = Object.values(params.exercises).map((exercise, i) => (
+
+      <Text key={exerciseKeys[i]}>{exercise.name} <Text>Reps: {exercise.reps}</Text></Text>
+  ))
+
+  return(
+    <View style={styles.container}>
+      <Text style={styles.workoutTitles}>{params.title}</Text>
+      <Text style={styles.workoutTitles}>{params.originator}</Text>
+      <Text style={styles.workoutTitles}>Description:</Text>
+      <Text style={styles.workoutInfo}>{params.desc}</Text>
+      <Text style={styles.workoutTitles}>Exercises: </Text>
+      <View>
+        {exerciseList}
       </View>
-    )
-  }
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#00FFFF',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 5,
   },
+  workoutTitles: {
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  workoutInfo: {
+    fontWeight: "normal",
+    textAlign: "left",
+  }
 });
